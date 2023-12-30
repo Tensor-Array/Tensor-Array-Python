78a22b1c8b152d2e5c41947a93a01e0cdeb55010
Noob <63889503+BigNoobWasTaken@users.noreply.github.com>
2023-12-30 14:15:17 +0700

    test

--- PythonTensorTesting/PyTensorArray/__init__.py
+++ PythonTensorTesting/PyTensorArray/__init__.py
@@ -1,17 +1,21 @@
-from typing import Self
 import numpy as np
 import tensor as t
 
 class Tensor:
-    def __init__(self, arr, *args):
+    def __init__(self, arr):
         self.temp_tensor = t.TensorC(arr)
         
-    def __add__(self, other) -> Self:
+    def __add__(self, other):
         result = Tensor(0);
         result.temp_tensor = self.temp_tensor + other.temp_tensor
         return result;
 
-    def __matmul__(self, other) -> Self:
+    def __sub__(self, other):
+        result = Tensor(0);
+        result.temp_tensor = self.temp_tensor - other.temp_tensor
+        return result;
+
+    def __matmul__(self, other):
         result = Tensor(0);
         result.temp_tensor = self.temp_tensor @ other.temp_tensor
         return result;
--- PythonTensorTesting/main.py
+++ PythonTensorTesting/main.py
@@ -9,7 +9,7 @@ import numpy as np
 if __name__ == '__main__':
     t3 = np.array([[1, 2, 3], [4, 5, 6]])
     t1 = py_t_arr.Tensor([[1, 2.5, 3], [4, 5, 6]])
-    t2 = py_t_arr.Tensor([[1, 2.5], [4, 5], [7, 8]])
+    t2 = py_t_arr.Tensor([[1, 2.5], [4, 5], [7.5, 8]])
     t3 = t1 @ t2
     print("Hello")
     print(t3)
