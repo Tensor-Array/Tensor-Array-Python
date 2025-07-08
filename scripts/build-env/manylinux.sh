cb2ad46c5aa5bcfd75df96e47c3b646192a840e9
Noob <63889503+BigNoobWasTaken@users.noreply.github.com>
2025-07-08 21:28:29 +0700

    test

--- scripts/build-env/manylinux.sh
+++ scripts/build-env/manylinux.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not. 
+if [ "$is_root" = false ] && [ "$has_sudo" = false ]; then 
+    echo "Root or sudo is required. Aborting."
+    exit 1
+elif [ "$is_root" = false ] ; then
+    USE_SUDO=sudo
+else
+    USE_SUDO=
+fi
+
 cd ../..
 
 # Install dependencies for building Tensor-Array on manylinux
 chmod +x tensor-array-repo/Tensor-Array/scripts/actions/install-cuda-rhel.sh
-dnf -y install redhat-lsb-core wget
+$USE_SUDO dnf -y install redhat-lsb-core wget
 tensor-array-repo/Tensor-Array/scripts/actions/install-cuda-rhel.sh
 
 # Check if nvcc is available
